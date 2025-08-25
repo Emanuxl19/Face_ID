67cf906d96f0a1f0361ec694c1a1f03a45c96c35
Emanuel Azevedo <azevedoemanuel630@gmail.com>
2025-08-25 12:29:52 -0300

    Delete src/main directory

--- src/main/java/com/faceid/exception/InvalidCredentialsException.java
+++
@@ -1,7 +0,0 @@
-package com.faceid.exception;
-
-public class InvalidCredentialsException extends RuntimeException {
-    public InvalidCredentialsException(String message) {
-        super(message);
-    }
-}
